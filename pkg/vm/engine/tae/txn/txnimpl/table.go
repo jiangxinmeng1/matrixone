ba31efd0da58ba0b3fc5675dd44aa90df1d5d4ce
jiangxinmeng <jiangxinmeng@sjtu.edu.cn>
2023-07-24 14:46:46 +0800

    add debug log

--- pkg/vm/engine/tae/txn/txnimpl/table.go
+++ pkg/vm/engine/tae/txn/txnimpl/table.go
@@ -781,9 +781,8 @@ func (tbl *txnTable) TryDeleteByDeltaloc(id *common.ID, deltaloc objectio.Locati
 	node2, ok, err := blkData.TryDeleteByDeltaloc(tbl.store.txn, deltaloc)
 	if err == nil && ok {
 		if err = tbl.UpdateDeltaLoc(id, deltaloc); err != nil {
-			ok = false
-			err = nil
-			return
+			logutil.Infof("err is nil")
+			return false, nil
 		}
 		logutil.Infof("blk %v, delete length %d", id.BlockID.String(), node2.GetCardinalityLocked())
 		if err = tbl.AddDeleteNode(id, node2); err != nil {
